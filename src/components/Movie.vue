af1d3d5519a3dea409fc4bdd61c37b7d251d34a6
Andreea1696 <andreeapetrescu16@gmail.com>
2021-12-01 16:57:27 +0100

    subcomponent created

---
+++ src/components/Movie.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="card" >
+        <span>Titolo: {{ details.title }} </span>
+        <span>Titolo originale: {{ details.original_title }}</span>
+        <span>Lingua: {{ details.original_language }}</span>
+        <span>Voto: {{ details.vote_average }}</span>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Movie',
+  props: {
+      details: Object,
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.card {
+    margin: 20px;
+
+    span {
+        display: block;
+    }
+}
+</style>
