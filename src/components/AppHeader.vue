fdba764ee05acf2e10309d76622da90fc479bce6
Andreea1696 <andreeapetrescu16@gmail.com>
2021-12-01 12:24:24 +0100

    file macrostructure

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="header-wrapper">
+      <form action="">
+          <input type="text" placeholder="Search movie">
+          <button>Search</button>
+      </form>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
