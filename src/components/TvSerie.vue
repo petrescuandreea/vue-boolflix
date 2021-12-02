c6622f1b24e94bb823ee32bfca6e5c2b27eaab54
Andreea1696 <andreeapetrescu16@gmail.com>
2021-12-02 14:46:03 +0100

    component created

---
+++ src/components/TvSerie.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="card" >
+        <span>Titolo: {{ details.name }} </span>
+        <span>Titolo originale: {{ details.original_name }}</span>
+        <img v-if="details.original_language === 'it'" src='../assets/img/it.jpg'  alt="Italian Flag">
+        <img v-else-if="details.original_language === 'en'" src='../assets/img/en.jpg'  alt="English Flag">
+        <span v-else>Lingua originale non disponibile</span>
+        <span>Voto: {{ details.vote_average }}</span>
+    </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'TvSerie',
+  props: {
+      details: Object,
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.card {
+    margin: 20px;
+
+    span {
+        display: block;
+    }
+
+    img {
+        width: 25px;
+    }
+}
+</style>
---
+++ src/components/TvSeries.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="cards-container">
+      <TvSerie  
+      v-for="tvSerie in selectedTvSerie"
+      :key="tvSerie.id" 
+      :details="tvSerie"
+      />
+  </div>
+</template>
+
+<script>
+
+import TvSerie from '@/components/TvSerie.vue';
+
+export default {
+  name: 'TvSeries',
+  components: {
+    TvSerie,
+  },
+  props: {
+      selectedTvSerie: Array,
+  },
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
