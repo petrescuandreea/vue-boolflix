97511aae2d7fac2b08dec31a8927c39e50d281d4
Andreea1696 <andreeapetrescu16@gmail.com>
2021-12-02 14:47:10 +0100

    milestone 2.2 - Tv Series

--- src/components/ParentComponent.vue
+++ src/components/ParentComponent.vue
@@ -3,13 +3,14 @@
       <header>
       <!-- componente AppHeader  -->
       <!-- catturo l'evento lanciato da AppHeader => emit -->
-      <AppHeader @searchMovie="searchedMovie"/>
+      <AppHeader @search="searched" />
     </header>
 
     <main>
-      <!-- componente AppMain  -->
-      <!-- invio il dato ricevuto da AppHeader a AppMain => props -->
-      <AppMain :selectedMovie="listMovies"/>
+      <!-- componente Movies / TvSeries -->
+      <!-- invio il dato ricevuto da AppHeader a Movies e TvSeries=> props -->
+      <Movies :selectedMovie="listMovies"/>
+      <TvSeries :selectedTvSerie="listTvSeries"/>
 
     </main>
   </div>
@@ -18,45 +19,62 @@
 <script>
 import axios from 'axios';
 import AppHeader from '@/components/AppHeader.vue';
-import AppMain from '@/components/AppMain.vue';
+import Movies from '@/components/Movies.vue';
+import TvSeries from '@/components/TvSeries.vue';
 
 export default {
   name: 'ParentComponent',
   components: {
     AppHeader,
-    AppMain,
+    Movies,
+    TvSeries,
   },
   data() {
       return {
         //   salvo il dato inviato da AppHeader  
+        //   film 
           choosedMovie: "",
+
+        //   serie tv 
+          choosedTvSerie: "",
 
         //   proprietà per creare l'URL
           urlMovies: "https://api.themoviedb.org/3/search/movie",
+          urlTvSerie: "https://api.themoviedb.org/3/search/tv",
           apiKey: "16cc96ba36cde80dcf11f479cce8e348",
 
           listMovies: [],
+          listTvSeries: [],
       }
 
   },
   methods: {
       getMovies() {
-        //   richiesta axios 
+        //   richiesta axios per film
         axios
         .get(`${this.urlMovies}?api_key=${this.apiKey}&language=it-IT&query=${this.choosedMovie}`)
         .then((result) => {
             this.listMovies = result.data.results;
-            console.log(this.listMovies);
+            console.log("movies" , result.data.results);
         });
-
-        // azzerro il valore dell'input 
-        this.choosedMovie = "";
       },
 
-      // tengo traccia del film richiesto dall'utente
-      searchedMovie(movies) {
+      getTvSeries () {
+           //   richiesta axios per serie tv
+        axios
+        .get(`${this.urlTvSerie}?api_key=${this.apiKey}&language=it-IT&query=${this.choosedMovie}`)
+        .then((result) => {
+            this.listTvSeries = result.data.results;
+            console.log("tvSeries" , result.data.results);
+        });
+
+      },
+
+      // tengo traccia del film/serie cercata dall'utente
+      searched(movies) {
           this.choosedMovie = movies;
           this.getMovies();
+          this.getTvSeries();
       },
         
   }
